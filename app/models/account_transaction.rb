8fd97e870dfc6e6aa1291f93378f9daad6326492
Babs Craig <babscraig@gmail.com>
2017-08-30 04:52:37 +0100

    Add models for bank account, client and transfer

--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -1,3 +1,18 @@
 class AccountTransaction < ApplicationRecord
   belongs_to :bank_account
+
+  TRANSACTION_TYPES = [ "withdrawal", "deposit" ]
+
+  validates :bank_account, presence: true
+  validates :amount, numericality: true, presence: true
+  validates :transanction_type, presence: true, inclusion: { in: TRANSATION_TYPES }
+  validates :transaction_number, preence: true, uniqueness: true
+
+  before_validation :load_defaults
+
+  def load_defaults
+    if self.new_record?
+      self.transaction_number = SecureRandom.uuid
+    end
+  end
 end
